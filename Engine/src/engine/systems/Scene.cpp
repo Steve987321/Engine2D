b835556655abbc508415b402e7f32c0748d7a208
Steve987321 <Stevebudding123@gmail.com>
2023-09-17 18:35:54 +0200

    removed commented old functions

--- Engine/src/engine/systems/Scene.cpp
+++ Engine/src/engine/systems/Scene.cpp
@@ -43,40 +43,6 @@ Object* Scene::GetSceneObject(std::string_view obj_name)
 	return nullptr;
 }
 
-//Object* Scene::AddToScene(Circle&& obj)
-//{
-//	auto objName = obj.name;
-//	if (objectsMap.contains(objName))
-//	{
-//		objName += " (" + std::to_string(objectsMap.count(objName)) + ')';
-//		while (objectsMap.contains(objName))
-//		{
-//			objName += " (" + std::to_string(objectsMap.count(objName)) + ')';
-//		}
-//	}
-//
-//	obj.name = objName;
-//	objectsMap.insert({objName, std::make_shared<Circle>(obj)});
-//	return objectsMap[objName].get();
-//}
-//
-//Object* Scene::AddToScene(Sprite&& obj)
-//{
-//	auto objName = obj.name;
-//	if (objectsMap.contains(objName))
-//	{
-//		objName += " (" + std::to_string(objectsMap.count(objName)) + ')';
-//		while (objectsMap.contains(objName))
-//		{
-//			objName += " (" + std::to_string(objectsMap.count(objName)) + ')';
-//		}
-//	}
-//
-//	obj.name = objName;
-//	objectsMap.insert({ objName, std::make_shared<Sprite>(obj) });
-//	return objectsMap[objName].get();
-//}
-
 bool Scene::RemoveFromScene(std::string_view obj_name)
 {
 	if (objectsMap.contains(obj_name.data()))
