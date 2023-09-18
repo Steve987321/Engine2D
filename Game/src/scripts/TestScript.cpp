08b48e253385e636930a1545e6983cbb76eec2b3
Steve987321 <Stevebudding123@gmail.com>
2023-09-18 21:08:02 +0200

    game project can now use sfml and engine functions

--- Game/src/scripts/TestScript.cpp
+++ Game/src/scripts/TestScript.cpp
@@ -2,7 +2,7 @@
 #include "game_core/GameBase.h"
 #include "TestScript.h"
 
-#include "engine/object_default/Object.h"
+#include "Engine/Engine.h"
 
 using namespace Toad;
 
@@ -10,5 +10,12 @@ void TestScript::OnUpdate(Object* obj)
 {
 	Script::OnUpdate(obj);
 
+	auto circle = dynamic_cast<Circle*>(obj);
+	if (circle != nullptr)
+	{
+		static sf::Clock clock;
+		
+		LOGDEBUG("TestScript: {}", clock.getElapsedTime().asMilliseconds());
+	}
 	obj->name = "Test Script is doing this";
 }
