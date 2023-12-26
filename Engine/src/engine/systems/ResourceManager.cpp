4d1fbae97a72e333331f5ce01306f209250284be
Steve987321 <Stevebudding123@gmail.com>
2023-12-26 01:28:20 +0100

    ResourceManager implementation for textures & ENGINE API

--- Engine/src/engine/systems/ResourceManager.cpp
+++ Engine/src/engine/systems/ResourceManager.cpp
@@ -6,13 +6,39 @@
 
 namespace Toad
 {
-	sf::Texture* ResourceManager::AddTexture(const sf::Texture& tex)
+	sf::Texture* ResourceManager::AddTexture(std::string_view id, const sf::Texture& tex)
 	{
-		m_textures.emplace_back(tex);
-		return &m_textures.back();
+		if (m_textures.contains(id.data()))
+		{
+			LOGWARNF("[ResourceManager] Texture {} is getting replaced", id.data());
+		}
+		m_textures[id.data()] = sf::Texture(tex);
+		return &m_textures[id.data()];
 	}
 
-	std::vector<sf::Texture>& ResourceManager::GetTextures()
+	sf::Texture* ResourceManager::GetTexture(std::string_view id)
+	{
+		return &m_textures[id.data()];
+	}
+
+	bool ResourceManager::RemoveTexture(std::string_view id)
+	{
+		if (auto it = m_textures.find(id.data()); 
+			it != m_textures.end())
+		{
+			m_textures.erase(it);
+			return true;
+		}
+
+		return false;
+	}
+
+	void ResourceManager::Clear()
+	{
+		m_textures.clear();
+	}
+
+	std::unordered_map<std::string, sf::Texture>& ResourceManager::GetTextures()
 	{
 		return m_textures;
 	}
--- Engine/src/engine/systems/ResourceManager.h
+++ Engine/src/engine/systems/ResourceManager.h
@@ -3,16 +3,21 @@
 namespace Toad
 {
 
-class ResourceManager
+class ENGINE_API ResourceManager
 {
 public:
 	ResourceManager() = default;
 
-	std::vector<sf::Texture>& GetTextures();
-	sf::Texture* AddTexture(const sf::Texture& tex);
+	std::unordered_map<std::string, sf::Texture>& GetTextures();
+	sf::Texture* AddTexture(std::string_view id, const sf::Texture& tex);
+	sf::Texture* GetTexture(std::string_view id);
+	bool RemoveTexture(std::string_view id);
+
+	void Clear();
 
 private:
-	std::vector<sf::Texture> m_textures;
+	std::unordered_map<std::string, sf::Texture> m_textures;
+	//std::unordered_map<std::string, sf::Font> m_fonts;
 };
 
 }
