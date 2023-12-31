43aca5dabc16850c76258687795bb2f7694b929b
Steve987321 <Stevebudding123@gmail.com>
2023-12-31 21:32:49 +0100

    added Vec3 & AudioSource

--- Engine/src/engine/Types.h
+++ Engine/src/engine/Types.h
@@ -3,3 +3,12 @@
 using Vec2i = sf::Vector2i;
 using Vec2f = sf::Vector2f;
 using Vec2u = sf::Vector2u;
+using Vec3i = sf::Vector3i;
+using Vec3f = sf::Vector3f;
+
+struct AudioSource
+{
+	std::filesystem::path full_path;
+	std::filesystem::path relative_path;
+	sf::SoundBuffer sound_buffer;
+};
