bfaf4de482f19c4a5f6298a5bb4ed05459fca51d
Steve987321 <Stevebudding123@gmail.com>
2024-03-01 17:41:53 +0100

    Implemented Input class

--- Engine/src/engine/systems/Input.cpp
+++ Engine/src/engine/systems/Input.cpp
@@ -1,2 +1,69 @@
 #include "pch.h"
 #include "Input.h"
+
+namespace Toad
+{
+
+	bool Input::IsKeyDown(sf::Keyboard::Key key)
+	{
+		return sf::Keyboard::isKeyPressed(key);
+	}
+
+	bool Input::IsMouseDown(sf::Mouse::Button button)
+	{
+		return sf::Mouse::isButtonPressed(button);
+	}
+
+	void Input::AddKeyPressCallback(FKEY_CALLBACK callback)
+	{
+		m_keyPressCallbacks.emplace_back(callback);
+	}
+
+	void Input::AddMousePressCallback(FMOUSE_CALLBACK callback)
+	{
+		m_mousePressCallbacks.emplace_back(callback);
+	}
+
+	void Input::AddKeyReleaseCallback(FKEY_CALLBACK callback)
+	{
+		m_keyUpCallbacks.emplace_back(callback);
+	}
+
+	void Input::AddMouseReleaseCallback(FMOUSE_CALLBACK callback)
+	{
+		m_mouseUpCallbacks.emplace_back(callback);
+	}
+
+	void Input::InvokeKeyPressCallbacks(sf::Keyboard::Key key)
+	{
+		for (const auto& c : m_keyPressCallbacks)
+		{
+			c(key);
+		}
+	}
+
+	void Input::InvokeMousePressCallbacks(sf::Mouse::Button button)
+	{
+		for (const auto& c : m_mousePressCallbacks)
+		{
+			c(button);
+		}
+	}
+
+	void Input::InvokeKeyReleaseCallbacks(sf::Keyboard::Key key)
+	{
+		for (const auto& c : m_keyUpCallbacks)
+		{
+			c(key);
+		}
+	}
+
+	void Input::InvokeMouseReleaseCallbacks(sf::Mouse::Button button)
+	{
+		for (const auto& c : m_mouseUpCallbacks)
+		{
+			c(button);
+		}
+	}
+
+}
--- Engine/src/engine/systems/Input.h
+++ Engine/src/engine/systems/Input.h
@@ -2,15 +2,39 @@
 
 #include "EngineCore.h"
 
-#include "engine/Engine.h"
-
 // interface for scripts to handle inputs 
 
 // also handles added callbacks for key presses or releases 
 
-class ENGINE_API Input
+namespace Toad
 {
-public:
 
-};
+	class ENGINE_API Input
+	{
+	public:
+		friend class Engine;
 
+		typedef void(*FKEY_CALLBACK)(sf::Keyboard::Key);
+		typedef void(*FMOUSE_CALLBACK)(sf::Mouse::Button);
+
+		static bool IsKeyDown(sf::Keyboard::Key key);
+		static bool IsMouseDown(sf::Mouse::Button button);
+
+		static void AddKeyPressCallback(FKEY_CALLBACK callback);
+		static void AddMousePressCallback(FMOUSE_CALLBACK callback);
+		static void AddKeyReleaseCallback(FKEY_CALLBACK callback);
+		static void AddMouseReleaseCallback(FMOUSE_CALLBACK callback);
+
+	private:
+		static void InvokeKeyPressCallbacks(sf::Keyboard::Key key);
+		static void InvokeMousePressCallbacks(sf::Mouse::Button button);
+		static void InvokeKeyReleaseCallbacks(sf::Keyboard::Key key);
+		static void InvokeMouseReleaseCallbacks(sf::Mouse::Button button);
+
+		inline static std::vector<FKEY_CALLBACK> m_keyPressCallbacks{};
+		inline static std::vector<FMOUSE_CALLBACK> m_mousePressCallbacks{};
+		inline static std::vector<FKEY_CALLBACK> m_keyUpCallbacks{};
+		inline static std::vector<FMOUSE_CALLBACK> m_mouseUpCallbacks{};
+	};
+
+}
