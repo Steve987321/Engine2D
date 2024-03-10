79a2674304f1ce1b66fde2212da7193dc083aa91
Steve987321 <Stevebudding123@gmail.com>
2024-03-10 15:32:29 +0100

    Create Package fixes and more checks

--- Engine/src/engine/systems/build/Package.cpp
+++ Engine/src/engine/systems/build/Package.cpp
@@ -19,42 +19,71 @@ namespace Toad
 	{
 	}
 
-	bool Package::CreatePackage(const fs::path& project_file, const fs::path& output_path, const fs::path& build_system_path, const fs::path& engine_path)
+	bool Package::CreatePackage(const fs::path& project_file_path, const fs::path& output_dir_path, const fs::path& build_system_file_path, const fs::path& engine_path)
 	{
-		const fs::path proj_dir = project_file.parent_path();
+		const fs::path proj_dir = project_file_path.parent_path();
+		const fs::path proj_bin_path = proj_dir / "bin" / "Release-windows-x86_64";
+		const fs::path out_dir = output_dir_path / "packaged_game";
 		const fs::path& proj_engine_dir = engine_path;
+
 		if (proj_engine_dir.empty())
 		{
 			LOGERROR("[Package] engine path is empty");
 			return false;
 		}
 
-		fs::path slnfile_dir;
-
+		fs::path proj_assets_path;
 		for (const auto& entry : fs::directory_iterator(proj_dir))
 		{
-			if (entry.path().has_extension() && entry.path().extension() == ".sln")
+			if (entry.path().filename().string().find("_Game") != std::string::npos)
 			{
-				slnfile_dir = entry;
+				proj_assets_path = entry.path() / "src" / "assets";
+				if (!fs::exists(proj_assets_path))
+				{
+					LOGERROR("[Package] No assets or src folder found in {}", proj_assets_path);
+					return false;
+				}
+
 				break;
 			}
 		}
 
-		const fs::path out_dir = output_path / "packaged_game";
-
-		if (project_file.extension() != FILE_EXT_TOADPROJECT)
+		if (proj_assets_path.empty())
 		{
-			LOGERRORF("Create package project file is invalid: {}", project_file);
+			LOGERRORF("[Package] No Game folder found in {}", proj_dir);
 			return false;
 		}
 
-		// delete log from before if exists
+		fs::path slnfile_path;
+		for (const auto& entry : fs::directory_iterator(proj_dir))
+		{
+			if (entry.path().has_extension() && entry.path().extension() == ".sln")
+			{
+				slnfile_path = entry;
+				break;
+			}
+		}
+
+		if (slnfile_path.empty())
+		{
+			LOGERRORF("[Package] No .sln file found in {}", project_file_path.parent_path());
+			return false;
+		}
+
+		if (project_file_path.extension() != FILE_EXT_TOADPROJECT)
+		{
+			LOGERRORF("[Package] Create package project file is invalid: {}", project_file_path);
+			return false;
+		}
+
+		// delete log from before
 		if (fs::exists(proj_dir / "buildlog.txt"))
 		{
 			fs::remove(proj_dir / "buildlog.txt");
 		}
 
-		if (!RunBuildSystemWithArgs(build_system_path, format_str("{} /build Release /out buildlog.txt", slnfile_dir.string())))
+		LOGDEBUGF("[Package] Running build for {}", slnfile_path);
+		if (!RunBuildSystemWithArgs(build_system_file_path, format_str("{} /build Release /out buildlog.txt", slnfile_path.string())))
 		{
 			LOGERROR("Failed to run build system");
 			return false;
@@ -66,13 +95,19 @@ namespace Toad
 		}
 
 		std::ifstream buildlog(proj_dir / "buildlog.txt");
-		std::atomic_bool build_done = false;
 
-		if (!buildlog.is_open())
+		if (!buildlog)
 		{
+			LOGERRORF("[Package] buildlog.txt can't be opened in {}", proj_dir);
 			return false;
 		}
 
+		std::atomic_bool build_done = false;
+		std::string build_finish_status;
+
+		std::set<std::string> build_progress_status;
+
+		// reads buildlog.txt
 		std::thread check_build_done([&]
 		{
 			while (!build_done)
@@ -80,44 +115,28 @@ namespace Toad
 				std::string line;
 				while (std::getline(buildlog, line))
 				{
-#ifdef TOAD_DISTRO
-					if (line.find("1 up-to-date") != std::string::npos)
+					if (!build_progress_status.contains(line))
 					{
-						build_done = true;
-						break;
+						LOGDEBUGF("[Package] Build Status: {}", line);
+						build_progress_status.insert(line);
 					}
-					if (line.find("========== Build: 0 succeeded, 1 failed") != std::string::npos)
+					if (line.find("========== Build: ") != std::string::npos)
 					{
-						build_done = true;
-						return false;
-					}
-
-					if (line.find("========== Build: 1 succeeded") != std::string::npos
-						&& line.find("0 failed") != std::string::npos)					
-#else
-					if (line.find("========== Build: 3 succeeded") != std::string::npos
-						&& line.find("0 failed") != std::string::npos)
-#endif 
-					{
+						build_finish_status = line;
 						build_done = true;
 						break;
 					}
 				}
-				static std::string line_last;
-				if (line_last != line)
-					LOGDEBUGF("[Package] {}", line);
-				line_last = line;
 				buildlog.clear();
 				buildlog.seekg(0);
-				std::this_thread::sleep_for(std::chrono::milliseconds(500));
+				std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 			}
-
 		});
 
 		while (!build_done)
 		{
 			// building status ..
-			std::this_thread::sleep_for(std::chrono::milliseconds(100));
+			std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 		}
 
 		if (check_build_done.joinable())
@@ -125,25 +144,54 @@ namespace Toad
 			check_build_done.join();
 		}
 
-		fs::create_directory(out_dir);
-
-		for (const auto& entry : fs::directory_iterator(proj_dir))
+		// check build status 
+		if (build_finish_status.find("0 failed") == std::string::npos)
 		{
-			if (!entry.is_directory())
-			{
-				continue;
-			}
+			LOGERRORF("[Package] Experienced errors while building {}", slnfile_path);
+			return false;
+		}
 
-			if (entry.path().filename().string().find("_Game") != std::string::npos)
+		// check output binaries 
+		bool has_dll = false;
+		for (const auto& entry : fs::directory_iterator(proj_bin_path))
+		{
+			if (entry.path().extension() == ".dll")
 			{
-				fs::copy(entry.path() / "src" / "assets", out_dir, fs::copy_options::recursive);
-
+				has_dll = true;
 				break;
 			}
 		}
 
+		if (!has_dll)
+		{
+			LOGERRORF("[Package] No game dll found in {}", proj_bin_path);
+			return false;
+		}
+
+		if (!fs::exists(proj_dir / "assets"))
+		{
+			LOGERRORF("[Package] No assets folder in {}", proj_dir);
+			return false;
+		}
+
+		fs::create_directory(out_dir);
+
+		// #TODO add compression
+		// copy assets (no scripts)
+		for (const auto& entry : fs::recursive_directory_iterator(proj_assets_path))
+		{
+			// skip source script files 
+			if (entry.path().extension() == ".cpp" || entry.path().extension() == ".h")
+				continue;
+
+			if (!fs::copy_file(entry.path(), out_dir / fs::relative(entry.path(), proj_assets_path)))
+			{
+				LOGERRORF("[Package] Failed to copy {} to {}", entry.path(), out_dir / out_dir / fs::relative(entry.path(), proj_assets_path));
+			}
+		}
+
 #ifdef TOAD_DISTRO
-		for (const auto& entry : fs::directory_iterator(proj_dir / "bin" / "Release-windows-x86_64"))
+		for (const auto& entry : fs::directory_iterator(proj_bin_path))
 		{
 			if (entry.path().extension() == ".dll")
 			{
@@ -163,7 +211,7 @@ namespace Toad
 		fs::path bin = proj_engine_dir / "bin";
 
 		// copy Runner No Editor
-		std::ifstream proj_file_f(project_file);
+		std::ifstream proj_file_f(project_file_path);
 		try {
 			json data = json::parse(proj_file_f);
 			std::string gamename = data["name"];
@@ -171,7 +219,7 @@ namespace Toad
 		}
 		catch (json::parse_error& e)
 		{
-			LOGWARNF("Failed to read project file {}", project_file);
+			LOGWARNF("Failed to read project file {}", project_file_path);
 			fs::copy_file(bin / "ToadRunnerNoEditor.exe", out_dir / "ToadRunnerNoEditor.exe");
 		}
 
@@ -187,7 +235,8 @@ namespace Toad
 			}
 		}
 #else 
-		for (const auto& entry : fs::directory_iterator(proj_dir / "bin" / "Release-windows-x86_64"))
+		// has both runner and game 
+		for (const auto& entry : fs::directory_iterator(proj_bin_path))
 		{
 			if (entry.path().extension() == ".exe" || entry.path().extension() == ".dll")
 			{
--- Engine/src/engine/systems/build/Package.h
+++ Engine/src/engine/systems/build/Package.h
@@ -12,11 +12,11 @@ public:
 	Package();
 	~Package();
 
+	// bool CreateDebugPackage() or PackageFlags & IsDebugBuild
 	bool CreatePackage(const std::filesystem::path& project_path, const std::filesystem::path& output_path, const std::filesystem::path& build_system_path, const std::filesystem::path& engine_path);
 
 private:
 	bool RunBuildSystemWithArgs(const std::filesystem::path& build_program, std::string_view args);
-
 };
 
 }
