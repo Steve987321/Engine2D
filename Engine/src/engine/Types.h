8688037408503e530fcc2d291415ccd00eafb27e
vierkant <Stevebudding123@gmail.com>
2024-08-03 16:38:06 +0200

    Added FloatRect & IntRect

--- Engine/src/engine/Types.h
+++ Engine/src/engine/Types.h
@@ -7,6 +7,46 @@ using Vec3i = sf::Vector3i;
 using Vec3f = sf::Vector3f;
 using Mouse = sf::Mouse;
 using Keyboard = sf::Keyboard;
+using IntRect = sf::IntRect;
+
+class FloatRect : public sf::FloatRect
+{
+public:
+	FloatRect()
+		: sf::FloatRect()
+	{}
+
+	FloatRect(const sf::FloatRect& r)
+	{
+		width = r.width;
+		height = r.height;
+		left = r.left;
+		top = r.top;
+	}
+
+public:
+	void Expand(float v)
+	{
+		width += v;
+		height += v;
+		left -= v;
+		top -= v;
+	}
+
+	bool Intersects(const FloatRect& other)
+	{
+		return intersects(other);
+	}
+
+	bool Contains(const Vec2f& point)
+	{
+		return point.x > left && point.x < left + width && point.y > top && point.y < top + height;
+	}
+
+private:
+	using sf::FloatRect::intersects;
+
+};
 
 struct AudioSource
 {
