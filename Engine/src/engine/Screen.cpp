0449febf059cbe3ee16cd740180e543ec6553a17
vierkant <Stevebudding123@gmail.com>
2024-08-08 09:20:38 +0200

    Fixed ScreenToWorld & Added overload for camera

--- Engine/src/engine/Screen.cpp
+++ Engine/src/engine/Screen.cpp
@@ -9,7 +9,21 @@ namespace Screen
     Vec2f ScreenToWorld(const Vec2i& point)
     {
         sf::RenderTexture& tex = Engine::Get().GetActiveRenderTexture();
-        return tex.mapPixelToCoords(point);
+
+        Camera* current_cam = Engine::Get().interacting_camera;
+        if (!current_cam) 
+            return {-1, -1};
+
+        return ScreenToWorld(point, *current_cam);
+    }
+
+    Vec2f ScreenToWorld(const Vec2i& point, const Camera& cam)
+    {
+        sf::RenderTexture& tex = Engine::Get().GetActiveRenderTexture();
+        float fx = cam.GetOriginalSize().x / cam.GetSize().x;
+        float fy = cam.GetOriginalSize().y / cam.GetSize().y;
+        Vec2i fixed_point = {(int)((float)point.x * fx), (int)(float)(point.y * fy)};
+        return tex.mapPixelToCoords(fixed_point);
     }
 }
 }
--- Engine/src/engine/Screen.h
+++ Engine/src/engine/Screen.h
@@ -2,8 +2,13 @@
 
 namespace Toad
 {
+
+class Camera;
+
 namespace Screen
 {
     Vec2f ScreenToWorld(const Vec2i& point);
+    Vec2f ScreenToWorld(const Vec2i& point, const Camera& cam);
 }
+
 }
